4edfd6307b652f676f50e0b37c2ad0e326751780
Jay Sojitra <jaysojitra13@gmail.com>
2022-03-24 09:10:33 +0530

    create partial.js

---
+++ partial.ts
@@ -0,0 +1,18 @@
+type User = {
+  name: string,
+  age: number
+};
+
+// This line will make all the fields optional inside type
+type PartialUser = Partial<User>;
+
+// scenario: 1
+const user: PartialUser = {
+  "name": "a",
+  "age": 19
+}
+
+// scenario: 2
+const user: PartialUser = {
+  "name": "a"
+}
