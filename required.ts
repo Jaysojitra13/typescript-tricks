c86722b49a965eb59b7b5a33791214631428cae1
Jay Sojitra <jaysojitra13@gmail.com>
2022-03-24 09:13:08 +0530

    Create required.ts

---
+++ required.ts
@@ -0,0 +1,13 @@
+type User = {
+  name: string,
+  age?: number, // optional
+  gender?: string // optional
+};
+
+// Below line will make all the fields required from above type
+type RequiredUser = Required<User>;
+  
+// scenario 1 - false
+const user: RequiredUser = {
+  "name": "a"  
+}
